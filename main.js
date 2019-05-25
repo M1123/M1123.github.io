4c687e069b99dbd60ea7b50d99142110e9d7e826
M1123 <dbv93@yandex.ru>
2019-05-25 14:27:16 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+$(()=>{
+    $("#add").on("click", ()=>{
+        let key= new Date();
+        let val=prompt('Заметка');
+        localStorage.setItem(key,val);
+        location.reload();
+    })
+    for (let i=0;i<localStorage.length;i++){
+        let key = localStorage.key(i);
+        let elem=$('<div class="card"></div>');
+        $(elem).append('<div class="card-body">'+localStorage.getItem(key)+'</div>');
+        $(elem).append('<div class="card-footer bg-transparent"><small class="text-muted">'+key+'</small></div>');
+        $(elem).append('<button class="del">x</button>');
+        $("#myList").append(elem);
+    }
+    $(".del").on("click",function(){      
+        let key = $(this).siblings().children().text();
+        localStorage.removeItem(key);
+        location.reload();
+    })
+    
+    $(".card").on(
+        "click", function(){
+            let key = $(this).children().eq(1).text();
+            console.log(key);
+            let val=prompt('Заметка');
+            localStorage.setItem(key,val);
+            location.reload();
+        }
+    )
+})
