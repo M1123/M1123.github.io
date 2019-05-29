f4f2774c91a3214c72bbf262e2f11877db22c4de
M1123 <dbv93@yandex.ru>
2019-05-30 01:33:18 +0700

    crud

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 $(()=>{
     $("#add").on("click", ()=>{
-        let key= new Date();
+        let dat= new Date(); 
+            var D = '' + dat.getDay();
+            D = D.length<2 ? '0' + D:D;
+            var M = '' + dat.getMonth();
+            M = M.length<2 ? '0' + M:M;
+            var Y = '' + dat.getFullYear();
+            var H = '' + dat.getHours();
+            H = H.length<2 ? '0' + H:H;
+            var I = '' + dat.getMinutes();
+            I = I.length<2 ? '0' + I:I;
+            var S = '' + dat.getSeconds();
+            S = S.length<2 ? '0' + S:S;
+            var toDay = Y + '.' + M + '.' + D;
+            var clock = H + ':' + I + ':' + S;
+        let key= toDay+" "+clock;
         let val=prompt('Заметка');
         localStorage.setItem(key,val);
         location.reload();
@@ -10,18 +24,18 @@ $(()=>{
         let elem=$('<div class="card"></div>');
         $(elem).append('<div class="card-body">'+localStorage.getItem(key)+'</div>');
         $(elem).append('<div class="card-footer bg-transparent"><small class="text-muted">'+key+'</small></div>');
-        $(elem).append('<button class="del">x</button>');
+        $(elem).append('<button class="body__button del">&#128939</button>');
+        $(elem).append('<button class="body__button edit">&#x270e</button>');
         $("#myList").append(elem);
     }
     $(".del").on("click",function(){      
         let key = $(this).siblings().children().text();
         localStorage.removeItem(key);
         location.reload();
-    })
-    
-    $(".card-body").on(
+    }) 
+    $(".edit").on(
         "click", function(){
-            let key = $(this).siblings().text();
+            let key = $(this).siblings().eq(1).text();
             console.log(key);
             let val=prompt('Заметка');
             localStorage.setItem(key,val);
