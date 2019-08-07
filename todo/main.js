f0e9f9cc4809f88c18e6ad068f54355ac6ada990
M1123 <dbv93@yandex.ru>
2019-08-08 02:58:19 +0700

    onblurEdit

--- todo/main.js
+++ todo/main.js
@@ -5,6 +5,7 @@ const colors=[
     'bg-danger',
     'bg-warning'
 ]
+
 let i=0;
 $(()=>{
     $("#add").on("click", ()=>{
@@ -31,45 +32,41 @@ $(()=>{
     for (let i=0;i<localStorage.length;i++){
         let key = localStorage.key(i);
         let array = JSON.parse(localStorage.getItem(key));
-        let bg=array[1]
-        console.log(colors[bg]);
-        let elem=$('<div class="card '+colors[bg]+'"></div>');
-        $(elem).append('<div class="card-body">'+array[0]+'</div>');
-        $(elem).append('<div class="card-footer"><small class="text-muted">'+key+'</small></div>');
-        $(elem).append('<button class="body__button del">&#128939</button>');
-        $(elem).append('<button class="body__button edit">&#x270e</button>');
-
-        $("#myList").append(elem);
+        $("#myList").append(`<div class="card ${colors[array[1]]}">
+        <div class="card-body" contenteditable>${array[0]}</div>
+        <button class="body__button del">&#128939</button>
+        <button class="body__button chbg">🖌</button>
+        <footer class="card-footer"><small>${key}</small></footer></div>`);
     }
     $(".del").on("click",function(){      
         let key = $(this).siblings().children().text();
         localStorage.removeItem(key);
         location.reload();
     }) 
-    $(".edit").on(
-        "click", function(){
-            let key = $(this).siblings().eq(1).text();
-            let array = JSON.parse(localStorage.getItem(key));
-            array[0]=prompt('Заметка',array[0]);
-            console.log(array[1]);
-            
-            localStorage.setItem(key,JSON.stringify(array));
-            location.reload();
-        }
+    // $(".edit").on("click", function(){
+    //     let key = $(this).siblings().eq(2).text();
+    //     let array = JSON.parse(localStorage.getItem(key));
+    //     array[0]=prompt('Заметка',array[0]);            
+    //     localStorage.setItem(key,JSON.stringify(array));
+    //     location.reload();
+    //     }
+    // )
+    $(".card-body").on("blur",function(){
+        let key = $(this).siblings().eq(2).text();
+        let array = JSON.parse(localStorage.getItem(key));
+        array[0]=$(this).text();            
+        localStorage.setItem(key,JSON.stringify(array));
+    }
+        
     )
-    $(".card-body").on(
-        "click", function(){
-            let key = $(this).siblings().eq(0).text();
-            console.log(key);
-            
-            let array = JSON.parse(localStorage.getItem(key));
-            let bg= array[1];
-            bg++;
-            array[1]=bg%5;
-            console.log(bg);
-            
-            localStorage.setItem(key,JSON.stringify(array));
-            location.reload();
+    $(".chbg").on("click", function(){
+        console.log($(this).siblings());
+        
+        let key = $(this).siblings().eq(2).text();
+        let array = JSON.parse(localStorage.getItem(key));
+        array[1]=(array[1]+1)%5;
+        localStorage.setItem(key,JSON.stringify(array));
+        location.reload();
         }
     )
 })
