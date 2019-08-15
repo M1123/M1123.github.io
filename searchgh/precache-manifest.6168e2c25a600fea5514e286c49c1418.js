dd7328a360a91f88685d3f65cab8820359f18498
M1123 <dbv93@yandex.ru>
2019-08-16 00:02:35 +0700

    add searchgh project

---
+++ searchgh/precache-manifest.6168e2c25a600fea5514e286c49c1418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce603bbbcd1d890d4d7a86526d38da3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa1a7dba66659d09f6a0",
+    "url": "/static/css/main.d08f14ca.chunk.css"
+  },
+  {
+    "revision": "0820e53ca5cec53eb931",
+    "url": "/static/js/2.b24205da.chunk.js"
+  },
+  {
+    "revision": "aa1a7dba66659d09f6a0",
+    "url": "/static/js/main.4f8b6bfd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
